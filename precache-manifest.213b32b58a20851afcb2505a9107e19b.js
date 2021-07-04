a4c4df811bddeef58b7947060f6b8b46bd4eacc2
mila1412 <80014554+mila1412@users.noreply.github.com>
2021-07-04 18:14:25 +0000

    Deploying to gh-pages from @ mila1412/Pomodoro@925299e9af96cbc2147771c04f6222c50676a7b0 🚀

---
+++ precache-manifest.213b32b58a20851afcb2505a9107e19b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b6781e4610e0aa28b6",
+    "url": "css/app.498dc4bf.css"
+  },
+  {
+    "revision": "0a1e4491ce25e4e351e6",
+    "url": "css/chunk-vendors.294bf131.css"
+  },
+  {
+    "revision": "9af944f4c5bf2f06178f3a83d5e4969e",
+    "url": "index.html"
+  },
+  {
+    "revision": "31b6781e4610e0aa28b6",
+    "url": "js/app.8b9de95b.js"
+  },
+  {
+    "revision": "0a1e4491ce25e4e351e6",
+    "url": "js/chunk-vendors.fc27ef4d.js"
+  },
+  {
+    "revision": "5af2880c8d584e1c7de8",
+    "url": "js/list.34b3b61b.js"
+  },
+  {
+    "revision": "048bb850fe8128ac097a",
+    "url": "js/settings.27d19dcd.js"
+  },
+  {
+    "revision": "4225dc2c01aeb4adc0f7b208f07c1729",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "1e1e82823174d4070e95c0eea06b666b",
+    "url": "media/Bluesy.1e1e8282.mp3"
+  },
+  {
+    "revision": "65eae6a1a4de5895859d2b29d75cd9c6",
+    "url": "media/Cartoon.65eae6a1.mp3"
+  },
+  {
+    "revision": "91f7b9d53dcba80f7b3c2fd56eb916e4",
+    "url": "media/Cheer.91f7b9d5.mp3"
+  },
+  {
+    "revision": "ba1adf5c215f49cb672194171e0a9e5a",
+    "url": "media/Cold.ba1adf5c.mp3"
+  },
+  {
+    "revision": "d05f3bf3aae0fb14fbc386b738c1b063",
+    "url": "media/Cruise.d05f3bf3.mp3"
+  },
+  {
+    "revision": "95a714ef3418ef95f6d03ef9d6227cb6",
+    "url": "media/Danzon.95a714ef.mp3"
+  },
+  {
+    "revision": "5a0177eb3628c4ffe86070b7eae4344f",
+    "url": "media/Dizzy.5a0177eb.mp3"
+  },
+  {
+    "revision": "8f129a171da0f6310bbee00e806a893a",
+    "url": "media/Doug.8f129a17.mp3"
+  },
+  {
+    "revision": "f0635e402379a68ced4f5fe73b9e21da",
+    "url": "media/Guitar.f0635e40.mp3"
+  },
+  {
+    "revision": "fb508cd7e17eda8ecd035fc1aa13e6ca",
+    "url": "media/Left.fb508cd7.mp3"
+  },
+  {
+    "revision": "5079073d5fdd9a0de51f50e8339cee91",
+    "url": "media/Loping.5079073d.mp3"
+  },
+  {
+    "revision": "3130c673bd3fedf8913f66ac32e871cd",
+    "url": "media/Mess.3130c673.mp3"
+  },
+  {
+    "revision": "d35a977afa9c3ff0b541474e0afec49c",
+    "url": "media/Puppy.d35a977a.mp3"
+  },
+  {
+    "revision": "590735e0615d067547031ffab932b62c",
+    "url": "media/Slay.590735e0.mp3"
+  },
+  {
+    "revision": "5c4c55064da3907b931556781a572f63",
+    "url": "media/Sour.5c4c5506.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
